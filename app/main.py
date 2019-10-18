b04516fd0c97c47f524ad951831348d277105f37
Juan Ramirez <juan.ramirez@capek.ai>
2019-10-17 19:25:12 -0500

    Add table

--- app/main.py
+++ app/main.py
@@ -2,8 +2,11 @@ import datetime
 
 from flask import Flask, escape, request
 from flask import render_template
+from flask_socketio import SocketIO, emit
+
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 
 @app.route('/')
@@ -17,6 +20,7 @@ def hello_name(name=None):
     name = request.args.get('name', 'Estudiante')
     return render_template(
         'hello.html',
+        url=name.replace(' ', '_'),
         name=name,
         date=str(datetime.datetime.now().date())
     )
@@ -25,4 +29,12 @@ def hello_name(name=None):
 @app.route('/shift_system/')
 def shift_system(name=None):
     name = request.args.get('name', 'Estudiante')
-    return render_template('shift_system.html', name=name)
+    date = request.args.get('date', '--')
+    return render_template(
+        'shift_system.html',
+        name=name.replace('_', ' '),
+        date=date
+    )
+
+if __name__ == '__main__':
+    socketio.run(app)
